7663170d1b32f2f84f863514ee98dd2e2056814d
JesusHerrera <jesus.herrera.90@gmail.com>
2014-09-02 23:27:26 -0300

    initial commit

---
+++ spec/lib/prototype_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe 'Prototype' do
+
+  context 'when a basic test example is requiered' do
+    it 'returns true' do 
+      expect(true).to be_truthy
+    end
+  end
+
+end
